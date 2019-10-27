dfb577952da0ae195c2eab73860130197aa2c11a
Vadim Galanov <vgalanov@ascendix.com>
2019-10-27 21:54:33 +0200

    magnet link support

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const config = require('./config');
 const Telegraf = require('telegraf');
 const Telegram = require('telegraf/telegram')
+const Transmission = require('transmission')
 const https = require('https');
 const fs = require('fs');
 
+const transmission = new Transmission();
 const bot = new Telegraf(config.botToken);
 const telegram = new Telegram(config.botToken)
 bot.start((ctx) => {
-    return ctx.reply('Welcome! Please drop torrent files here.');
+    return ctx.reply('Welcome! Please send magnet link or drop torrent files here.');
 });
 bot.on('message', (ctx) => {
+    console.log(ctx.message);
     if (ctx.message.document) {
         telegram.getFileLink(ctx.message.document.file_id).then(fileLink => {
             let fileName = fileLink.split('/').pop();
@@ -21,13 +24,26 @@ bot.on('message', (ctx) => {
                         response.pipe(newFile);
                     });
                     ctx.reply('Torrent file downloaded!');
-                } catch(ex) {
+                } catch (ex) {
                     ctx.reply('File download error.');
                 }
             } else {
-                ctx.reply('This is not a torrent file');
+                ctx.reply('This is not a torrent file!');
             }
         })
+    } else if (ctx.message.text) {
+        const magnetRegex = /^magnet.+$/i;
+        if (magnetRegex.text(ctx.message.text)) {
+            transmission.addUrl(ctx.message.text, (err, arg) => {
+                if (err) {
+                    ctx.reply("Link parse error");
+                } else {
+                    ctx.reply('Magnet link is parsed!');
+                }
+            });
+        } else {
+            ctx.reply('This is not a magnet link!');
+        }
     }
 });
 bot.launch();
