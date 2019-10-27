0b816abdf48797680516ff329ddb9c6e56fa0908
Vadim Galanov <vgalanov@ascendix.com>
2019-10-27 20:22:57 +0200

    bot added

--- config-template.js
+++ config-template.js
@@ -1,3 +1,4 @@
 module.exports = {
-    botToken: "<enter bot auth token>"
+    botToken: "<enter bot auth token>",
+    torrentsFolderPath: "<an absolute path to torrents input folder>"
 }
--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const config = require('./config');
 const Telegraf = require('telegraf');
+const Telegram = require('telegraf/telegram')
+const https = require('https');
+const fs = require('fs');
+
 const bot = new Telegraf(config.botToken);
+const telegram = new Telegram(config.botToken)
 bot.start((ctx) => {
-    return ctx.reply('Добро пожаловать!');
+    return ctx.reply('Welcome! Please drop torrent files here.');
+});
+bot.on('message', (ctx) => {
+    if (ctx.message.document) {
+        telegram.getFileLink(ctx.message.document.file_id).then(fileLink => {
+            let fileName = fileLink.split('/').pop();
+            let extension = fileName.split('.').pop();
+            if (extension === 'torrent') {
+                try {
+                    const newFile = fs.createWriteStream(`${config.torrentsFolderPath}/${fileName}`);
+                    https.get(fileLink, (response) => {
+                        response.pipe(newFile);
+                    });
+                    ctx.reply('Torrent file downloaded!');
+                } catch(ex) {
+                    ctx.reply('File download error.');
+                }
+            } else {
+                ctx.reply('This is not a torrent file');
+            }
+        })
+    }
 });
 bot.launch();
